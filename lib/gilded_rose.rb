838b980c5e0a63a5df5f6ba96c1517d3135c7a72
Benedict Lawrence <BTLawrence44@gmail.com>
2021-02-09 13:39:03 +0000

    reducing the method update_quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,29 +5,37 @@ class GildedRose
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" ? item.quality = item.quality - 1 : nil
-      else
+    @items.each do |item| 
+
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != "Sulfuras, Hand of Ragnaros" &&  item.quality > 0
+          item.name == "Conjured Mana Cake" ? item.quality = item.quality - 2 : item.quality = item.quality - 1 
+        end
+      else  
+
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality = item.quality + 1 
+          
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
               end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
+            end 
+
+            if item.sell_in < 6 && item.quality < 50
                 item.quality = item.quality + 1
-              end
-            end
+            end 
+
           end
-        end
-      end
+        end  
+
+      end 
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
-      end
+      end 
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,85 @@
 #require File.join(File.dirname(__FILE__), 'gilded_rose') 
 require 'gilded_rose'
 
-describe GildedRose do
+describe GildedRose do 
 
-  describe "#update_quality" do
+  
+
+  describe "#update_quality" do 
+
+    #name of items 
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq("foo")
-    end
+    end  
+
+    #sell in of items 
+
+    it 'does not change the sell_in' do 
+      item = [Item.new("foo", 4, 0)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].sell_in).to eq(3)
+    end   
+
+    #quality of items 
+
+    it 'quality reduces by one each day' do 
+      item = [Item.new("foo", 5, 5)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(4)
+    end    
+
+    #quality does not go below zero
+
+    it 'quality does not go into the negitve' do 
+      item = [Item.new("foo", 5, 0)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(0)
+    end 
+
+    #testing products from here 
+
+    it 'Aged Brie should increase in quality' do  
+      item = [Item.new("Aged Brie", 2, 0)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(1)
+    end   
+
+    it 'Backstage passes should increase in quality by 2 after 10 days or less' do 
+      item = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 20)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(22)
+    end  
+
+    it 'Backstage passes should increase in quality by 3 after 5 days or less' do 
+      item = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 20)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(23)
+    end 
+
+    it 'Backstage passes quality should drop to zero when the consent is finished' do 
+      item = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 20)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(0)
+    end  
+
+    # testing non perishable items  
+
+    it 'Sulfures, Hand of Ragaros should not reduce in quality' do
+      item = [Item.new("Sulfuras, Hand of Ragnaros", 0, 49)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(49)
+    end 
+
+    # testing Conjured items 
+
+    it 'Conjured Mana Cake should decrease quality twise as fast' do 
+      item = [Item.new("Conjured Mana Cake", 10, 10)] 
+      GildedRose.new(item).update_quality() 
+      expect(item[0].quality).to eq(8)
+    end 
+
   end
 end
