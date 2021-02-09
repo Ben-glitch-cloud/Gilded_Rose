2c31e982536163948c5aa5bb5d66391f6da0647b
Benedict Lawrence <BTLawrence44@gmail.com>
2021-02-09 11:23:57 +0000

    first test complete

--- gilded_rose_if.rb
+++ gilded_rose_if.rb
@@ -1,17 +1,11 @@
-class GildedRose
-
-  def initialize(items)
-    @items = items
-  end
-
-  def update_quality()
+def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
-        end
+        end 
       else
         if item.quality < 50
           item.quality = item.quality + 1
@@ -50,19 +44,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
